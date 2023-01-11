cc6e0b3fc93634a82c74a7376b9559c524a8779b
Ehsan <egolkar@gmail.com>
2023-01-11 11:00:30 -0500

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,20 @@
+from compari import main
+from setuptools import setup, find_packages
 
+VERSION = '0.0.1'
+DESCRIPTION = 'A package to model peritumoal context '
+
+
+setup(
+    name="convrsn",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Ehsan Golkar",
+    author_email="egolkar@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='conversion',
+
+)
