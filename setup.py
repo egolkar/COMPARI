13511c590e64dbede0cee4bc14a12157b4d43f2e
Ehsan <egolkar@gmail.com>
2023-01-11 11:54:43 -0500

    Create setup.py
    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
+import os
+
+
+DESCRIPTION = 'A package to model crossing fiber in the peritumoral refion from dMRI' 
+
+setup(name='compari',
+    description=DESCRIPTION,
+    version='0.1.dev0',
+    author='Ehsan Golkar',
+    author_email='egolkar@gmail.com',
+    scripts = [ os.path.join('scripts', 'compari.py'),
+                ],
+    packages = ['compari',
+                ],
+    )
